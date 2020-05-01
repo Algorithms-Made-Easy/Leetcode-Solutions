09052c45acac632dc2266b29daf4694338c096df
Rajat Agarwal <36561091+rajatag03@users.noreply.github.com>
2020-05-01 17:03:50 +0530

    First Repeated and Unique char in String.

---
+++ FirstUniqChar.java
@@ -0,0 +1,16 @@
+class FirstUniqChar {
+    public int firstUniqChar(String s) {
+        int[] val=new int[26];
+        for(char c:s.toCharArray()){
+            val[c-'a']++;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(val[s.charAt(i)-'a']>1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
